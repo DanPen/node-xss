7f94773d21f039593d80cb4af1de25596fe62260
Daniel Pendergast <danielpendergast@icloud.com>
2015-06-18 01:52:18 -0500

    Switch to cheerio (jQuery) for parsing DOM. Implement anchor scraping

--- scan.js
+++ scan.js
@@ -1,108 +1,103 @@
-var http = require('http');
-var https = require('https');
-
-
 var flags = require('flags');
+var url = require('url');
 
 flags.defineString('url', 'www.google.com', 'URL of page to scan');
 flags.defineStringList('targets', ['form', 'object'], 'the HTML tags you want to search for');
 flags.parse();
 
-var url = flags.get('url');
-url = /(http:\/\/)?((?:www\.)?[^\/]+)(\/.+)?/.exec(url);
+var targetURL = flags.get('url');
+targetURL = /(http:\/\/)?((?:www\.)?[^\/]+)(\/.+)?/.exec(targetURL);
 
 var options = {
-  host : url[2],
+  host : targetURL[2],
   port : 80,
-  path : url[3]
+  path : targetURL[3]
 }
 
-var page = "";
-var tags = [];
+var http = require('http');
+
+var crawlQueue = [];
+
+var cheerio = require('cheerio');
 
 var req = http.request(options, function (res) {
 
   res.setEncoding('utf8');
 
-  var purgatory = '';
-
-  var readTagName = false;
-  var tagPurgatoryName = '';
-
-  var readTagFull = false;
-  var tagPurgatoryFull = '';
-
+  var page = '';
 
   res.on('data', function (chunk) {
-
-    var lastChar;
-
-    for (var i = 0, len = chunk.length; i < len; i++) {
-      // Start of beginning tag
-      if (chunk[i] == '<') {
-        readTagName = true;
-        readTagFull = true;
-      }
-
-      // Don't read end tags
-      if (chunk[i] == '/' && lastChar == '<') {
-        readTagName = false;
-        readTagFull = false;
-        tagPurgatoryFull = '';
-      }
-
-      if (readTagFull && chunk[i] != '>') {
-        tagPurgatoryFull += chunk[i];
-      }
-
-      if (readTagFull && tagPurgatoryFull && chunk[i] == '>') {
-        tagPurgatoryFull += chunk[i];
-
-        readTagName = false;
-        readTagFull = false;
-
-        gotTag(tagPurgatoryName, tagPurgatoryFull);
-
-        tagPurgatoryName = '';
-        tagPurgatoryFull = '';
-      }
-
-      if (readTagName && chunk[i] != '<' && chunk[i] != ' ' && chunk[i] != '/') {
-        tagPurgatoryName += chunk[i];
-      }
-
-      if (readTagName && (chunk[i] == ' ' || chunk[i] == '/')) {
-        readTagName = false;
-      }
-
-
-      // if (tagPurgatoryFull && readTagFull && chunk[i] == ' ') {
-      //   readTagFull = false;
-      //   console.log(tagPurgatoryFull);
-      //   tags.push(tagPurgatoryFull);
-      //   tagPurgatoryFull = '';
-      // }
-
-      lastChar = chunk[i];
-
-    }
-
     page += chunk;
-
   });
 
   res.on('end', function () {
+    $ = cheerio.load(page);
 
+    var anchors = scrapeAnchors($)
+    for (var i = 0; i < anchors.length; i++) {
+      console.log('Possible reflective XSS at %s', anchors[i].url)
+    }
+
+    // scrapeForms($);
   });
 
 }).end();
 
-function gotTag (tagName, tagFull) {
-  tags.push({
-    tagName : tagName,
-    tagFull : tagFull
+function scrapeAnchors ($) {
+
+  var hrefsWithQuery = [];
+
+  $('a').each(function () {
+
+    var href = $(this).attr('href');
+
+    if (href !== undefined) {
+
+      var urlParsed = url.parse(href, true, true);
+
+      var queries = urlParsed.query;
+
+      var len = Object.keys(queries).length;
+
+      if (len > 0) {
+
+        var hrefWithQuery = {
+          url : href.replace(urlParsed.search, ''),
+          keys : []
+        }
+
+        for (var i = 0; i < len; i++) {
+          hrefWithQuery.keys[i] = {
+            name : Object.keys(queries)[i],
+            possibleType : ''
+          };
+        }
+
+        hrefsWithQuery.push(hrefWithQuery);
+
+      }
+
+    }
+
+    if (href != '#' && href !== undefined)
+      crawlQueue.push(href);
+
   });
 
-  if (tagName == 'form')
-    console.log(tagFull);
+  return hrefsWithQuery;
+
 }
+
+
+
+}
+
+// Step 1: scan entire site for used query strings
+//  a: scrape HTML for anchors
+//    i: find hrefs with query strings
+//    ii: use all links for crawling and scraping those pages
+//  b: scrape HTML for forms
+//    i: find inputs in GET forms
+//    ii: find query strings other forms 'action'
+//  c: scrape HTML for inputs without forms
+//    i: run page in phantomjs and test these inputs for XHRs and check the resulting query strings
